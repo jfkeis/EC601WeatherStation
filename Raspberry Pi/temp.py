5a2fae64aa3672ae8829355ff8933378cc829c78
siggi89 <siggi.thorvalds@gmail.com>
2016-12-13 20:47:25 -0500

    cloned git repo to RaspberryPi and adding files from Pi

--- Raspberry Pi/temp.py
+++ Raspberry Pi/temp.py
@@ -1,4 +1,7 @@
-
+import requests
+import json
+import time
+from firebase import firebase
 
 tfile = open("/sys/bus/w1/devices/28-00000807fc61/w1_slave")
 
@@ -16,15 +19,24 @@ temperature = temperature / 1000
 
 print temperature
 
-import time
 timestr = time.strftime("%Y.%m.%d-%H.%M.%S")
-filename = str(timestr)+'.txt'
+filename = str(timestr)+'.json'
 timestamp = int(time.time())
-#print timestr
+parent = 'h'+str(timestamp)[:9]+'0'
+entry = '\'' + str(timestr) + '\''
+print parent
 
 
 with open(filename , "a") as myfile:
    	myfile.write('"currently":{'+'\n'+' "time":'+repr(timestamp)+ ','+'\n' +' "temp":'+repr(temperature)+'}'+'\n')
 
+#firebase_url = 'https://weathernow-3f8f4.firebaseio.com' #Siggi
+#firebase_url = 'https://weathernow-db3fe.firebaseio.com' #John
 
+firebase = firebase.FirebaseApplication('https://weathernow-db3fe.firebaseio.com/')
+resultPut = firebase.put('/temperature',parent,{'time':timestamp,'temp':temperature})
 
+data = {'time':timestamp,'temp':temperature}
+#result = requests.post(firebase_url + '/' + '/temperature.json', data=json.dumps(data))
+
+#result2 = request.post(firebase_url
